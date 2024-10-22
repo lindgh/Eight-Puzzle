81ff2a081743c1764cb4cc5d0d48d51110da7a84
AKethidi <48158707+AKethidi@users.noreply.github.com>
2024-10-22 16:59:23 -0700

    made main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //welcome the user 
+    cout << "Welcome to 862331573 and XXX's 8 puzzle solver." << endl;
+    cout << "Type “1” to use a default puzzle, or “2” to enter your own puzzle." << endl;
+
+
+    return 0;
+}
