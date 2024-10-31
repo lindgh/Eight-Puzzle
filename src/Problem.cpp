4ac9bde0e7024e612b410c966591d8dae25cf275
AKethidi <48158707+AKethidi@users.noreply.github.com>
2024-10-30 17:05:22 -0700

    updated header

--- src/Problem.cpp
+++ src/Problem.cpp
@@ -3,43 +3,46 @@
 #include <iostream>
 using namespace std;
 
-Node* make_node(int arr[N][N], int x, int y, int level){
-    Node* node = new Node;
+Node Problem::make_node(int arr[N][N], int x, int y, int level){
+    Node node = new Node;
     node->arr = arr;
 
 }
 
-//operators 
-//down
-//int x and int y are the coordinates for wherever 0 is
-Node Problem::down(Node arr[N][N], int x, int y){
-    //new node called here, copy of the parent
-    if(!(y+1 > 2)){
-        swap(arr[x][y], arr[x][y+1]);
-    }
-    return arr; //??
-}
-//up
-Node Problem::up(Node arr[N][N], int x, int y){
-    if(!(y-1 < 0)){
-        swap(arr[x][y], arr[x][y-1]);
-    }
-    return arr; //??
-}
-//left
-Node Problem::left(Node arr[N][N], int x, int y){
-    if(!(x-1 < 0)){
-        swap(arr[x][y], arr[x-1][y]);
-    }
-    return arr; //??
-}
-//right
-Node Problem::right(Node arr[N][N], int x, int y){
-    if(x+1 > 2){
-        swap(arr[x][y], arr[x+1][y-1]);
-    }
-    return arr; //??
-}
+// //operators 
+// //down
+// //int x and int y are the coordinates for wherever 0 is
+// Node Problem::down(Node arr[N][N], int x, int y){
+//     //new node called here, copy of the parent
+//     //copy of Node arr
+
+//     //edge  cases should be in explore()
+//     if(!(y+1 > 2)){
+//         swap(arr[x][y], arr[x][y+1]);
+//     }
+//     return arr; //??
+// }
+// //up
+// Node Problem::up(Node arr[N][N], int x, int y){
+//     if(!(y-1 < 0)){
+//         swap(arr[x][y], arr[x][y-1]);
+//     }
+//     return arr; //??
+// }
+// //left
+// Node Problem::left(Node arr[N][N], int x, int y){
+//     if(!(x-1 < 0)){
+//         swap(arr[x][y], arr[x-1][y]);
+//     }
+//     return arr; //??
+// }
+// //right
+// Node Problem::right(Node arr[N][N], int x, int y){
+//     if(x+1 > 2){
+//         swap(arr[x][y], arr[x+1][y-1]);
+//     }
+//     return arr; //??
+// }
 
 
 
